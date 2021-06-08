f9f52a7ef62885bd721c8161eefee6bd73e707f5
Mahesh Shivegowda <mshivegowda@empoweredbenefits.com>
2021-06-08 19:48:40 +0530

    update08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-First CICD-App';
+  title = 'My-First-App';
+  serverName = "Application Server";
 }
